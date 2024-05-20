bb1359a247c86e8758abef85a3439482bef46a03
Jelle De Vreeze <jelle.de.vreeze@telenet.be>
2024-05-20 21:26:23 +0200

    Quartz sync: May 20, 2024, 9:26 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -22,7 +22,7 @@ const config: QuartzConfig = {
       fontOrigin: "googleFonts",
       cdnCaching: true,
       typography: {
-        header: "Fredoka",
+        header: "Inter",
         body: "Open Sans",
         code: "IBM Plex Mono",
       },
