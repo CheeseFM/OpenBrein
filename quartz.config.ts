c8df791a20989dd0b180c7e79339c06401539cb2
cheesefm <jelle.de.vreeze@telenet.be>
2024-09-28 14:50:40 +0200

    Quartz sync: Sep 28, 2024, 2:50 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Openbrein ",
+    pageTitle: "Openbrein ,
     enableSPA: true,
     enablePopovers: true,
     analytics: {
@@ -22,7 +22,7 @@ const config: QuartzConfig = {
       fontOrigin: "googleFonts",
       cdnCaching: true,
       typography: {
-        header: "Montserrat",
+        header: "Montserrat Bold",
         body: "Montserrat",
         code: "IBM Plex Mono",
       },
